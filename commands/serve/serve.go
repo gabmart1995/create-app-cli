fc871723ff528b48b1de93a3cb1dff9176536417
gabmart1995 <gabmart1995@gmail.com>
2023-03-20 11:05:12 -0400

    wip: ajustes

--- commands/serve/serve.go
+++ commands/serve/serve.go
@@ -54,14 +54,6 @@ func serve(c *cli.Context) error {
 	port := c.Int("port")
 	directory := c.Path("directory")
 
-	printMessages := func(address string, port int) {
-
-		messageServer := fmt.Sprintf("Servidor estatico operando en la dirección: \"http://%s:%d\"", address, port)
-		fmt.Println("Este es un servidor de desarrollo. No usar en producción")
-		fmt.Println(messageServer)
-		fmt.Println("Pulsa Control + C para cerrar el servicio")
-	}
-
 	if helpers.FileNotExists(directory) {
 		return errors.New("no existe directorio donde mostrar los archivos en la web")
 	}
@@ -70,7 +62,10 @@ func serve(c *cli.Context) error {
 	fileServer := http.FileServer(http.Dir(directory))
 	http.Handle("/", http.StripPrefix("/", fileServer))
 
-	printMessages(address, port)
+	messageServer := fmt.Sprintf("Servidor estatico operando en la dirección: \"http://%s:%d\"", address, port)
+	fmt.Println("Este es un servidor de desarrollo. No usar en producción")
+	fmt.Println(messageServer)
+	fmt.Println("Pulsa Control + C para cerrar el servicio")
 
 	// si existe algun error desde la plataforma
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", address, port), nil))
